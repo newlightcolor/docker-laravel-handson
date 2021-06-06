d37db24b59044a287db83c9a04acd8b07b1da470
newlightcolor <newlightcolor@gmail.com>
2021-06-07 03:41:13 +0900

    release タスクのseeder作成

--- backend/laravel/database/seeders/TasksTableSeeder.php
+++ backend/laravel/database/seeders/TasksTableSeeder.php
@@ -2,17 +2,18 @@
 
 namespace Database\Seeders;
 
+use App\Models\Task;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class TasksTableSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call(TasksTableSeeder::class);
+        Task::factory()->count(3)->create();
     }
 }
