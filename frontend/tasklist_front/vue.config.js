459ed6e2422c56e6e520d4ce14ce83ae7c49a348
newlightcolor <newlightcolor@gmail.com>
2021-06-14 06:25:53 +0900

    release フロント切り分け　vue-cli4導入

---
+++ frontend/tasklist_front/vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    devServer: {
+      port: 8888,
+    },
+    configureWebpack: {
+      devServer: {
+        watchOptions: {
+          poll: true
+        }
+      }
+    }
+}
