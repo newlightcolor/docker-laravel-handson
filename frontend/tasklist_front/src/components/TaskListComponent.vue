599c49f9e774c8e8a97e5571f1401508a96174de
newlightcolor <newlightcolor@gmail.com>
2021-06-18 05:05:13 +0900

    release タスクの編集をタスク一覧から行えるように変更

--- frontend/tasklist_front/src/components/TaskListComponent.vue
+++ frontend/tasklist_front/src/components/TaskListComponent.vue
@@ -16,9 +16,20 @@
             </thead>
             <tbody>
             <tr v-for="(task, index) in tasks" :key="index">
-                <th scope="row">{{ task.id }}</th>
-                <td>{{ task.title }}</td>
-                <td style="word-break: break-all">{{ task.content }}</td>
+                <td scope="row">{{ task.id }}</td>
+                <td>
+                    <div v-show="editTask.id !== task.id">{{ task.title }}</div>
+                    <input type="text" class="form-control-plaintext"
+                            v-show="editTask.id === task.id"
+                            v-model="editTask.title">
+                </td>
+                <td style="word-break: break-all" v-on:fucus="editMode(task.id)">
+                    <div v-show="editTask.id !== task.id">{{ task.content }}</div>
+                    <textarea type="text" class="form-control-plaintext"
+                            v-show="editTask.id === task.id"
+                            v-model="editTask.content"
+                            style=""/>
+                </td>
                 <td>{{ task.person_in_charge }}</td>
                 <td>
                     <router-link v-bind:to="{name: 'task.show', params: {taskId: task.id }}">
@@ -26,9 +37,8 @@
                     </router-link>
                 </td>
                 <td>
-                    <router-link v-bind:to="{name: 'task.edit', params: {taskId: task.id }}">
-                        <button class="btn btn-success">Edit</button>
-                    </router-link>
+                    <button class="btn btn-success" v-show="editTask.id !== task.id" v-on:click="editMode(index)">Edit</button>
+                    <button class="btn btn-success" v-show="editTask.id === task.id" v-on:click="editModeFinish()">End</button>
                 </td>
                 <td>
                     <button class="btn btn-danger" v-on:click="deleteTask(task.id, index)">Delete</button>
@@ -45,7 +55,8 @@
     export default {
         data: function () {
             return {
-                tasks: []
+                tasks: [],
+                editTask: {},
             }
         },
         methods: {
@@ -61,13 +72,26 @@
             },
             catchTask(task) {
                 this.tasks.push(task)
-            }
+            },
+            editMode(index){
+                this.editTask = this.tasks[index]
+                this.editTask.index = index
+            },
+            editModeFinish(){
+                this.$axios.put('/api/tasks/'+ this.editTask.id, this.editTask)
+                    .then(() => {
+                        alert('koushin')
+                    }
+                )
+                this.tasks[this.editTask.index] = this.editTask
+                this.editTask = {}
+            },
         },
         mounted() {
             this.getTasks();
         },
         components: {
             postTaskForm
-        }
+        },
     }
 </script>
